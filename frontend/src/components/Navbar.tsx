fb4879797d24df0b4a2a8c242c1e027e72f2826a
krzyfok <k.fokczynski@gmail.com>
2026-06-03 14:42:12 +0200

    navbar fix

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,5 +1,3 @@
-
-
 import { NavLink, useNavigate } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext';
 
@@ -7,7 +5,7 @@ const navLinkClass = ({ isActive }: { isActive: boolean }) =>
   `text-gray-700 hover:text-blue-600 ${isActive ? 'text-blue-600 font-semibold border-b-2 border-blue-600' : ''}`;
 
 export function Navbar() {
-  const { logout, isAuthenticated } = useAuth();
+  const { logout, isAuthenticated, user } = useAuth();
   const navigate = useNavigate();
 
   const handleLogout = () => {
@@ -24,13 +22,23 @@ export function Navbar() {
         >
           TwójLekarz.pl
         </NavLink>
-        {isAuthenticated && (
+        {isAuthenticated && user?.role == "patient" &&(
           <div className="flex items-center space-x-4">
             <NavLink to="/find-doctor" className={navLinkClass}>
               Znajdź Lekarza
             </NavLink>
             <NavLink to="/appointments" className={navLinkClass}>
               Wizyty
+            </NavLink>
+          </div>
+        )}
+        {isAuthenticated && user?.role == "doctor" &&(
+          <div className="flex items-center space-x-4">
+            <NavLink to="/doctor-appointment" className={navLinkClass}>
+              Twój grafik przyjęć
+            </NavLink>
+            <NavLink to="/edit-profile" className={navLinkClass}>
+              Edytuj profil
             </NavLink>
           </div>
         )}
